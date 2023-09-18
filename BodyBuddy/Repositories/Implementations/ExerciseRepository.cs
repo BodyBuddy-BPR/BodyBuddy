57a64344ab63a3ffc3b9e77357fac0be62c18a36
Frederik Marker Hansen <frederik.m.hansen@gmail.com>
2023-09-18 12:14:59 +0200

    Exercise now works all the way through

--- BodyBuddy/Repositories/Implementations/ExerciseRepository.cs
+++ BodyBuddy/Repositories/Implementations/ExerciseRepository.cs
@@ -22,11 +22,17 @@ namespace BodyBuddy.Repositories.Implementations
 
         public async Task<List<Exercise>> GetExercisesAsync(string category, string musclegroup)
         {
+            string lowerCategory = category.ToLower(); 
+            string lowerMusclegroup = musclegroup.ToLower();
+
             try
             {
+                //var exercises = await _context.Table<Exercise>()
+                //    .Where(x => x.Category.Equals(category, StringComparison.OrdinalIgnoreCase) &&
+                //    x.PrimaryMuscles.Equals(musclegroup, StringComparison.OrdinalIgnoreCase)).ToListAsync();
+
                 var exercises = await _context.Table<Exercise>()
-                    .Where(x => x.Category.Equals(category, StringComparison.OrdinalIgnoreCase) &&
-                    x.PrimaryMuscles.Equals(musclegroup, StringComparison.OrdinalIgnoreCase)).ToListAsync();
+                    .Where(x => x.Category == lowerCategory && x.PrimaryMuscles == lowerMusclegroup).ToListAsync();
 
                 return exercises;
             }
@@ -59,8 +65,13 @@ namespace BodyBuddy.Repositories.Implementations
                 Console.WriteLine($"Error in GetMuscleGroupsForCategory: {ex}");
                 return new List<string>(); // Return an empty list or handle the error gracefully.
             }
+        }
 
 
+        public async Task<Exercise> GetExerciseDetails(int id)
+        {
+            var exercise = await _context.Table<Exercise>().FirstOrDefaultAsync(x => x.Id == id);
+            return exercise;
         }
 
         #region Supabase methods
