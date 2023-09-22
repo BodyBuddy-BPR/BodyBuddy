69fc4672a9530c651819e4073ff5cf31092477f2
Frederik Marker Hansen <frederik.m.hansen@gmail.com>
2023-09-22 09:20:51 +0200

    Syncfusion License added

--- BodyBuddy/App.xaml.cs
+++ BodyBuddy/App.xaml.cs
@@ -4,7 +4,10 @@ public partial class App : Application
 {
 	public App()
 	{
-		InitializeComponent();
+        //Register Syncfusion license
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWXZccnRWR2RfV0V/XUs=");
+
+        InitializeComponent();
 
 		MainPage = new AppShell();
 	}
