6dcb721c5a4232c00dc6973a20d324b9c4a9a0d5
Frederik Marker Hansen <frederik.m.hansen@gmail.com>
2023-09-27 10:40:02 +0200

    ImageCaching

---
+++ BodyBuddy/Helpers/ExerciseImageHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BodyBuddy.Helpers
+{
+    public class ExerciseImageHelper : IValueConverter
+    {
+        private const string BaseImageUrl = "https://raw.githubusercontent.com/yuhonas/free-exercise-db/main/exercises/";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string imageString)
+            {
+                var imagePaths = imageString.Split(',').Select(path => path.Trim()).ToList();
+
+                var imagePath = BaseImageUrl + imagePaths[0];
+
+                var imageUrl = BaseImageUrl + imagePaths[0];
+
+                return new Uri(imageUrl);
+            }
+            return new Uri("no_image.png") ;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
