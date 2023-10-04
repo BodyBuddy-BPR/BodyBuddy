44cfbcdca4fafa7e0885b5e0ede677f2b06163c9
Sebastian Thomsen <280301@VIAUC.DK>
2023-10-04 09:58:36 +0200

    BPR-4 - temp

--- BodyBuddy/ViewModels/StartupTest/StartupTestViewModel.cs
+++ BodyBuddy/ViewModels/StartupTest/StartupTestViewModel.cs
@@ -13,106 +13,88 @@ namespace BodyBuddy.ViewModels.StartupTest
 {
     public partial class StartupTestViewModel : BaseViewModel
     {
+        #region ObservableProperties
         [ObservableProperty]
-        private string _questionaireText;
+        private bool _isNameVisible, _isGenderVisible, _isWeightVisible, _isHeightVisible;
+        [ObservableProperty]
+        private bool _isAgeVisible, _isActiveVisible, _isPassiveCalorieBurnVisible, _isGoalVisible;
+
+        [ObservableProperty]
+        private string _questionaireText, _forwardButtonText;
+
+        //Saved Properties
+        [ObservableProperty]
+        private string name;
+        
+        [ObservableProperty]
+        private double weight;
+        
+        [ObservableProperty]
+        private int height;
+
+        #endregion
 
         public StartupTestViewModel()
         {
             IsNameVisible = true;
-            QuestionaireText = "What is your GG";
+            QuestionaireText = "What is your Name?";
+            ForwardButtonText = "Next Question";
         }
+
+
 
         [RelayCommand]
         public void ButtonTest()
         {
-            var properties = new Dictionary<string, Func<bool>>
-        {
-            { "IsNameVisible", () => IsNameVisible },
-            { "IsGenderVisible", () => IsGenderVisible },
-            { "IsWeightVisible", () => IsWeightVisible },
-            { "IsHeightVisible", () => IsHeightVisible },
-            { "IsAgeVisible", () => IsAgeVisible },
-            { "IsActiveVisible", () => IsActiveVisible },
-            { "IsPassiveCalorieBurnVisible", () => IsPassiveCalorieBurnVisible },
-            { "IsGoalVisible", () => IsGoalVisible },
-        };
-            var activeProperty = properties.FirstOrDefault(kvp => kvp.Value()).Key;
+            var steps = new List<(string PropertyName, Func<bool> Getter, Action<bool> Setter, string NextQuestionText)>
+{
+    ("IsNameVisible", () => IsNameVisible, value => IsNameVisible = value, "What is your gender?"),
+    ("IsGenderVisible", () => IsGenderVisible, value => IsGenderVisible = value, "What is your weight?"),
+    ("IsWeightVisible", () => IsWeightVisible, value => IsWeightVisible = value, "What is your height?"),
+    ("IsHeightVisible", () => IsHeightVisible, value => IsHeightVisible = value, "What is your age?"),
+    ("IsAgeVisible", () => IsAgeVisible, value => IsAgeVisible = value, "How active are you?"),
+    ("IsActiveVisible", () => IsActiveVisible, value => IsActiveVisible = value, "What is your passive calorie burn?"),
+    ("IsPassiveCalorieBurnVisible", () => IsPassiveCalorieBurnVisible, value => IsPassiveCalorieBurnVisible = value, "What are your goals?"),
+    ("IsGoalVisible", () => IsGoalVisible, value => IsGoalVisible = value, "The end, well done!")
+};
 
-            switch (activeProperty)
+            for (int i = 0; i < steps.Count; i++)
             {
-                case "IsNameVisible":
-                    IsNameVisible = !IsNameVisible;
-                    IsGenderVisible = !IsGenderVisible;
-                    QuestionaireText = "What is your gender ?";
-                    break;
-                case
-                    "IsGenderVisible":
-                    IsGenderVisible = !IsGenderVisible;
-                    IsWeightVisible = !IsWeightVisible;
-                    QuestionaireText = "What is your weight?";
-                    break;
-                case
-                    "IsWeightVisible":
-                    IsWeightVisible = !IsWeightVisible;
-                    IsHeightVisible = !IsHeightVisible;
-                    QuestionaireText = "What is your height?";
-                    break;
-                case "IsHeightVisible":
-                    IsHeightVisible = !IsHeightVisible;
-                    IsAgeVisible = !IsAgeVisible;
-                    QuestionaireText = "What is your age?";
-                    break;
-                case "IsAgeVisible":
-                    IsAgeVisible = !IsAgeVisible;
-                    IsActiveVisible = !IsActiveVisible;
-                    QuestionaireText = "How active are you?";
-                    break;
-                case "IsActiveVisible":
-                    IsActiveVisible = !IsActiveVisible;
-                    IsPassiveCalorieBurnVisible = !IsPassiveCalorieBurnVisible;
-                    QuestionaireText = "What is your passive calorie burn?";
-                    break;
-                case "IsPassiveCalorieBurnVisible":
-                    IsPassiveCalorieBurnVisible = !IsPassiveCalorieBurnVisible;
-                    IsGoalVisible = !IsGoalVisible;
-                    QuestionaireText = "What is your goals?";
-                    break;
-                case "IsGoalVisible":
-                    break;
-                default:
-                    break;
+                if (steps[i].Getter())
+                {
+                    // Set the current flag to false
+                    steps[i].Setter(false);
 
+                    // If there's a next step, set its flag to true and update the question text
+                    if (i + 1 < steps.Count)
+                    {
+                        steps[i + 1].Setter(true);
+                        QuestionaireText = steps[i].NextQuestionText;
+                    }
+                    else
+                    {
+                        ForwardButtonText = steps[i].NextQuestionText;
+                    }
+
+                    return;
+                }
             }
+
         }
 
-        private void DisableAllText()
-        {
-            IsNameVisible = false;
-            IsGenderVisible = false;
-            IsWeightVisible = false;
-            IsHeightVisible = false;
-            IsAgeVisible = false;
-            IsActiveVisible = false;
-            IsPassiveCalorieBurnVisible = false;
-            IsGoalVisible = false;
-        }
+        //private void DisableAllText()
+        //{
+        //    IsNameVisible = false;
+        //    IsGenderVisible = false;
+        //    IsWeightVisible = false;
+        //    IsHeightVisible = false;
+        //    IsAgeVisible = false;
+        //    IsActiveVisible = false;
+        //    IsPassiveCalorieBurnVisible = false;
+        //    IsGoalVisible = false;
+        //}
 
 
-        [ObservableProperty]
-        private bool _isNameVisible;
-        [ObservableProperty]
-        private bool _isGenderVisible;
-        [ObservableProperty]
-        private bool _isWeightVisible;
-        [ObservableProperty]
-        private bool _isHeightVisible;
-        [ObservableProperty]
-        private bool _isAgeVisible;
-        [ObservableProperty]
-        private bool _isActiveVisible;
-        [ObservableProperty]
-        private bool _isPassiveCalorieBurnVisible;
-        [ObservableProperty]
-        private bool _isGoalVisible;
+
     }
 }
