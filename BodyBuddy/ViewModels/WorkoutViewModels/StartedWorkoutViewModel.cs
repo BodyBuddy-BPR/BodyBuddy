f10dca2053b584577585fdde7aca82cad9cdc418
Frederik Marker Hansen <frederik.m.hansen@gmail.com>
2023-10-04 18:56:01 +0200

    BPR-2 - Buttons to cycle through exercises

--- BodyBuddy/ViewModels/WorkoutViewModels/StartedWorkoutViewModel.cs
+++ BodyBuddy/ViewModels/WorkoutViewModels/StartedWorkoutViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using BodyBuddy.Models;
+using BodyBuddy.Repositories;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,19 +15,70 @@ namespace BodyBuddy.ViewModels.WorkoutViewModels
     [QueryProperty(nameof(WorkoutDetails), "Workout")]
     public partial class StartedWorkoutViewModel : BaseViewModel
     {
+        private readonly IWorkoutExercisesRepository _workoutExercisesRepository;
+
         // Query field for the started workout
         [ObservableProperty]
         private Workout _workoutDetails;
 
-        public StartedWorkoutViewModel()
+        [ObservableProperty]
+        private Exercise _displayedExercise;
+        public ObservableCollection<Exercise> Exercises { get; set; } = new ObservableCollection<Exercise>();
+
+
+        public StartedWorkoutViewModel(IWorkoutExercisesRepository workoutExercisesRepository)
         {
-            
+            _workoutExercisesRepository = workoutExercisesRepository;
         }
 
         public async Task Initialize()
         {
-
+            await GetExercisesFromWorkout();
         }
 
+        public async Task GetExercisesFromWorkout()
+        {
+            if (IsBusy) return;
+
+            try
+            {
+                IsBusy = true;
+
+
+                var workoutPlan = await _workoutExercisesRepository.GetExercisesInWorkout(WorkoutDetails.Id, false); // False for user made workouts
+
+                if (Exercises.Count != 0)
+                {
+                    Exercises.Clear();
+                }
+
+                foreach (var exercise in workoutPlan)
+                {
+                    Exercises.Add(exercise);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                await Shell.Current.DisplayAlert("Error!", $"Unable to get workout plans {ex.Message}", "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+
+                DisplayedExercise = Exercises.FirstOrDefault();
+            }
+        }
+
+        [RelayCommand]
+        public async Task NextExercise()
+        {
+
+        }
+        [RelayCommand]
+        public async Task PreviousExercise()
+        {
+
+        }
     }
 }
