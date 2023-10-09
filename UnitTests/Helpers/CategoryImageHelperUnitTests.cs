ea56852753936f7a99582f88d617313dbfa11a7f
Sebastian Thomsen <280301@VIAUC.DK>
2023-10-09 10:17:15 +0200

    BPR-21 - Unit Tests setup + FileLogger created

---
+++ UnitTests/Helpers/CategoryImageHelperUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests.Helpers
+{
+    [TestClass]
+    public class CategoryImageHelperUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ UnitTests/ViewModels/CategoryViewModelUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests.ViewModels
+{
+    [TestClass]
+    public class CategoryViewModelUnitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
