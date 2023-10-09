9e0fb3080da31fed098ae2f495d204a5c45c5a28
Sebastian Thomsen <280301@VIAUC.DK>
2023-10-09 12:12:54 +0200

    BPR-21 - Template created
    PreMadeWorkoutsViewModelTests created using NUnit and using Mocking
    Examples of mock return and verify expectations.

---
+++ UnitTests/Template.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class Template
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            Assert.True(true);
+        }
+    }
+}
