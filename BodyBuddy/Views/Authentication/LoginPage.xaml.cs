cef049318c9a93ee7be16e7647c2d5bb476c191b
Frederik Marker Hansen <frederik.m.hansen@gmail.com>
2023-10-24 17:12:05 +0200

    ui might be done

--- BodyBuddy/Views/Authentication/LoginPage.xaml.cs
+++ BodyBuddy/Views/Authentication/LoginPage.xaml.cs
@@ -1,27 +1,82 @@
 using BodyBuddy.ViewModels.Authentication;
-using Mopups.Interfaces;
+
 
 namespace BodyBuddy.Views.Authentication;
 
-public partial class LoginPage
+public partial class LoginPage : ContentPage
 {
     private LoginViewModel _viewModel;
     public LoginPage(LoginViewModel loginViewModel)
-	{
-		InitializeComponent();
+    {
+        InitializeComponent();
         _viewModel = loginViewModel;
         BindingContext = loginViewModel;
-	}
+    }
 
-    private void SignUp_Clicked(object sender, EventArgs e)
+    #region Sign In
+
+    private async void SignIn_Clicked(object sender, EventArgs e)
+    {
+        await _viewModel.Login();
+    }
+
+    private void GoToSignUp_Clicked(object sender, EventArgs e)
     {
         _viewModel.IsLogin = false;
         _viewModel.IsSignUp = true;
     }
 
-    private void Login_Clicked(object sender, EventArgs e)
+    #endregion
+
+    #region Sign Up
+
+    private async void SignUp_Clicked(object sender, EventArgs e)
+    {
+        if (!EmailValid.IsValid)
+        {
+            EmailError.Text = "Email is not valid";
+            EmailError.IsVisible = true;
+
+            // Start the timer to hide the error label after 3 seconds
+            StartErrorLabelTimer(EmailError);
+        }
+        if (!PasswordValid.IsValid)
+        {
+            PasswordError.Text = PasswordValid.Errors.FirstOrDefault().ToString();
+            PasswordError.IsVisible = true;
+
+            // Start the timer to hide the error label after 3 seconds
+            StartErrorLabelTimer(PasswordError);
+        }
+        else if (PasswordValid.IsValid && EmailValid.IsValid)
+        {
+            EmailError.IsVisible = false;
+            PasswordError.IsVisible = false;
+
+            await _viewModel.SignUp();
+        }
+
+    }
+
+    private void GoToLogin_Clicked(object sender, EventArgs e)
     {
         _viewModel.IsLogin = true;
         _viewModel.IsSignUp = false;
     }
+
+    #endregion
+
+    private async void StartErrorLabelTimer(Label errorLabel)
+    {
+        // Wait for 3 seconds
+        await Task.Delay(3000);
+
+        // Hide the error label if it is still visible
+        if (errorLabel.IsVisible)
+        {
+            errorLabel.IsVisible = false;
+        }
+    }
+
+   
 }
