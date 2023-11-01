2d3e3a86adc8e4d14c41a6a452b116b13d222c22
Nivq <N_i_v_q@hotmail.com>
2023-11-01 22:48:57 +0100

    BPR-11 - Simple Chart Page POC Updated Nuget Packages

---
+++ BodyBuddy/ViewModels/ChartViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BodyBuddy.Dtos;
+
+namespace BodyBuddy.ViewModels
+{
+    public class ChartViewModel
+    {
+        public ObservableCollection<ChartData> Data { get; set; }
+
+        public ChartViewModel()
+        {
+            Data = new ObservableCollection<ChartData>()
+            {
+                new ChartData { XValue = new DateTime(2023, 01, 01), YValue = 30 },
+                new ChartData { XValue = new DateTime(2023, 02, 01), YValue = 28 },
+                new ChartData { XValue = new DateTime(2023, 03, 01), YValue = 34 },
+                // Add more data as needed
+            };
+        }
+    }
+}
