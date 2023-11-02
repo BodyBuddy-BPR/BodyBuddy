f9b0a2daff4b166859a9d27c5b7adac8b16b5745
Nivq <N_i_v_q@hotmail.com>
2023-11-02 01:05:49 +0100

    BPR-11 - UI Changes to Charts Toggle graphs by clicking legend
    Created simulation data, to see how the graph feels

--- BodyBuddy/ViewModels/ChartViewModel.cs
+++ BodyBuddy/ViewModels/ChartViewModel.cs
@@ -12,11 +12,19 @@ namespace BodyBuddy.ViewModels
     public partial class ChartViewModel : BaseViewModel
     {
         [ObservableProperty] private List<ChartData> _data;
+        public List<ExerciseRecordsDto> WorkoutDataFromDb { get; set; } = new List<ExerciseRecordsDto>();
+
 
         public ChartViewModel()
         {
+            //Random data simulation
+            GeneratedDataSimulation(3, 2);
+            //HardcodeDataSimulation();
+
             Data = CalculateStats();
         }
+
+
 
         private List<ChartData> CalculateStats()
         {
@@ -36,14 +44,65 @@ namespace BodyBuddy.ViewModels
             return stats;
         }
 
-        public List<ExerciseRecordsDto> WorkoutDataFromDb { get; set; } = new List<ExerciseRecordsDto>
+        private void HardcodeDataSimulation()
         {
-            new ExerciseRecordsDto { Date = new DateTime(2023, 01, 01), Weight = 26, Set = 1, Reps = 10},
-            new ExerciseRecordsDto { Date = new DateTime(2023, 01, 01), Weight = 28, Set = 2, Reps = 10},
-            new ExerciseRecordsDto { Date = new DateTime(2023, 01, 01), Weight = 30, Set = 3, Reps = 10},
-            new ExerciseRecordsDto { Date = new DateTime(2023, 02, 01), Weight = 28, Set = 1, Reps = 10},
-            new ExerciseRecordsDto { Date = new DateTime(2023, 03, 01), Weight = 34, Set = 1, Reps = 10},
-        };
+            WorkoutDataFromDb.AddRange(new List<ExerciseRecordsDto>
+            {
+                new() { Date = new DateTime(2023, 01, 01), Weight = 26, Set = 1, Reps = 10 },
+                new() { Date = new DateTime(2023, 01, 01), Weight = 28, Set = 2, Reps = 10 },
+                new() { Date = new DateTime(2023, 01, 01), Weight = 30, Set = 3, Reps = 10 },
+                new() { Date = new DateTime(2023, 02, 01), Weight = 28, Set = 1, Reps = 10 },
+                new() { Date = new DateTime(2023, 02, 01), Weight = 28, Set = 2, Reps = 10 },
+                new() { Date = new DateTime(2023, 02, 01), Weight = 30, Set = 3, Reps = 10 },
+                new() { Date = new DateTime(2023, 03, 01), Weight = 30, Set = 1, Reps = 10 },
+                new() { Date = new DateTime(2023, 03, 01), Weight = 30, Set = 1, Reps = 10 },
+                new() { Date = new DateTime(2023, 03, 01), Weight = 32, Set = 1, Reps = 10 },
+                new() { Date = new DateTime(2023, 04, 01), Weight = 32, Set = 1, Reps = 10 },
+                new() { Date = new DateTime(2023, 04, 01), Weight = 32, Set = 1, Reps = 10 },
+                new() { Date = new DateTime(2023, 04, 01), Weight = 32, Set = 1, Reps = 10 },
+            });
+        }
 
+        private void GeneratedDataSimulation(int monthsOfTraining, int trainingAWeek)
+        {
+            Random rand = new Random();
+
+            int startingWeight = 26;
+            int weightIncrement = 1;
+            int maxWeightIncrement = 3;
+            DateTime startDate = new DateTime(2023, 01, 01);
+            DateTime endDate = startDate.AddMonths(monthsOfTraining);
+
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(7 / trainingAWeek))
+            {
+                int sessionsThisWeek = trainingAWeek;
+                for (int session = 0; session < sessionsThisWeek; session++)
+                {
+                    int sets = rand.Next(1, 6); // Randomly choose 1-5 sets
+
+                    for (int set = 1; set <= sets; set++)
+                    {
+                        int weight = startingWeight + weightIncrement * set;
+                        weight += rand.Next(-1, 2); // Small variation in weight
+
+                        int reps = rand.Next(8, 12); // Randomly choose 8-12 reps
+
+                        WorkoutDataFromDb.Add(new ExerciseRecordsDto
+                        {
+                            Date = date.AddDays(session * (7 / trainingAWeek)), // Distribute sessions over the week
+                            Weight = weight,
+                            Set = set,
+                            Reps = reps
+                        });
+                    }
+                }
+
+                // Incrementally increase the weight over time
+                if (date.Day % 15 == 0)
+                {
+                    weightIncrement += rand.Next(1, maxWeightIncrement);
+                }
+            }
+        }
     }
 }
