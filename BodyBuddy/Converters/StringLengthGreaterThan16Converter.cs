d7f786429b7f82dc0b062500e0ff6d2ff5c4ab0e
MikRunRas <92924105+MikRunRas@users.noreply.github.com>
2023-11-06 10:23:48 +0100

    BPR-25 - Updated the ExerciseDetailsPage UI, and made a new converter to check if a string is longer than 16 characters. Very specific converter, I know, but I needed it for removing secondary muscles groups if they were too long

---
+++ BodyBuddy/Converters/StringLengthGreaterThan16Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BodyBuddy.Converters
+{
+    public class StringLengthGreaterThan16Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is string str)
+            {
+                return str.Length <= 16;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
