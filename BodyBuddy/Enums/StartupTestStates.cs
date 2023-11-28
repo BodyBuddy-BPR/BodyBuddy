d32a79e96ac0c7b536ffde7ffc3d4e35b04e9a87
MikRunRas <92924105+MikRunRas@users.noreply.github.com>
2023-11-28 09:10:31 +0100

    BPR-66 - Fixed some namings

--- BodyBuddy/Enums/StartupTestStates.cs
+++ BodyBuddy/Enums/StartupTestStates.cs
@@ -16,7 +16,7 @@ namespace BodyBuddy.Enums
         BirthdaySelection,
         ActivitySelection,
         PassiveCalorieBurnEntry,
-        FocusAreas,
+        TargetAreas,
         GoalSelection,
         Done
     }
