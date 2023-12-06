726ff92871824d99d128dac44f4123c64d2419b8
Nivq <N_i_v_q@hotmail.com>
2023-12-06 17:49:18 +0100

    BPR-81 - Models created for online DB Supabase
    Intake and everything regarding workouts

---
+++ BodyBuddy/Models/Supabase/WorkoutSbModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Postgrest.Attributes;
+using Postgrest.Models;
+
+namespace BodyBuddy.Models.Supabase
+{
+    [Table("Workout")]
+
+    public class WorkoutSbModel : BaseModel
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("userId")]
+        public string UserId { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("preMade")]
+        public bool PreMade { get; set; }
+    }
+}
