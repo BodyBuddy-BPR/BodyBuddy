ce857dfe99350688512724171dfc05671c7c2c75
Nivq <N_i_v_q@hotmail.com>
2023-12-06 14:34:20 +0100

    BPR-81 - Added some Documentation to DateHelper date converters - Avoid confusing and issues when converting back and forth using Utc and Unspecified DateTime Kind

--- BodyBuddy/Helpers/DateHelper.cs
+++ BodyBuddy/Helpers/DateHelper.cs
@@ -4,7 +4,7 @@
     {
         private const string _datePreferencesKey = "LastFetchedDate";
         public static DateTime Today => DateTime.Today;
-        public static DateTime Now => DateTime.Now;
+        public static DateTime Now => DateTime.UtcNow.Date;
 
         private static readonly DateTime _epochStart = new(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
@@ -25,11 +25,24 @@
             return _epochStart.AddSeconds(epoch);
         }
 
+        /// <summary>
+        /// Used whenever converting DateTime something not self-created
+        /// Etc. from Supabase, DatePickers etc. as they are Unspecified
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
         public static long ConvertToEpochTimeAtMidnightUnspecified(DateTime dateTime)
         {
             //Adding 1 hour for it to be at midnight
-            return (long)(dateTime.ToUniversalTime() - _epochStart).TotalSeconds+3600;
+            return (long)(dateTime.ToUniversalTime() - _epochStart).TotalSeconds + 3600;
         }
+
+        /// <summary>
+        /// Used when converting DateTime from UTC
+        /// For example when converting <see cref="GetCurrentDayAtMidnight"/>
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
         public static long ConvertToEpochTimeAtMidnightUtc(DateTime dateTime)
         {
             return (long)(dateTime.ToUniversalTime() - _epochStart).TotalSeconds;
@@ -38,8 +51,7 @@
         //Get current date at midnight in UTC, and convert it to a timestamp
         public static long GetCurrentDayAtMidnight()
         {
-            DateTime currentDateTime = DateTime.UtcNow.Date;
-            return (long)(currentDateTime.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            return (long)Now.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
         }
 
         private static DateTime GetLastFetchedDate()
